b83d7826dcf6797ca9d96e395eb222c72d7a024f
Alexander Cole Mora <alu0100767421@ull.edu.es>
2017-03-06 12:11:01 +0000

    cambios

--- pages/main.php
+++ pages/main.php
@@ -14,7 +14,7 @@
        <!-- Aquí ponemos todo el código HTML de nuestra página restringida, desde <html> a </html>-->
 
 <!DOCTYPE html>
-<html>
+<html lang="es">
   <head>
 
     <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -24,23 +24,65 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="icon" type="../image/png" href="http://www.guiadeisora.org/corp/wp-content/themes/FoundationPress-master/assets/img/icons/favicon.ico" />
 
-
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
     <meta charset="utf-8">
     <title>Turismo - Base de datos</title>
   </head>
   <body>
 
-    <nav class="#424242 grey darken-3">
-      <div class="nav-wrapper">
-        <a href="#" class="brand-logo left"><img id="escudo-nav" src="..\images\Escudos\Escudo_AyuntamientoGuiadeIsora1.png" alt="Escudo Guía de Isora"></a>
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li><a href="http://www.guiadeisora.org/corp">Ayuntamiento</a></li>
-          <li><a href="http://www.guiadeisora.travel/turismo">Turismo</a></li>
-        </ul>
+  <header>
+
+
+    <nav class="#424242 grey darken-3 top-nav">
+      <div class="container">
+        <div class="nav-wrapper">
+          <a href="#" class="brand-logo left"><img id="escudo-nav" src="..\images\Escudos\Escudo_AyuntamientoGuiadeIsora1.png" alt="Escudo Guía de Isora"></a>
+          <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="http://www.guiadeisora.org/corp">Ayuntamiento</a></li>
+            <li><a href="http://www.guiadeisora.travel/turismo">Turismo</a></li>
+          </ul>
+          <ul id="mobile-demo" class="side-nav">
+            <li><a href="http://www.guiadeisora.org/corp">Ayuntamiento</a></li>
+            <li><a href="http://www.guiadeisora.travel/turismo">Turismo</a></li>
+          </ul>
+        </div>
       </div>
+
     </nav>
+
+
+  </header>
+
+  <main>
+      <div class="row">
+        <div class="col s3 ">
+          <ul class="menu">
+            <li><a href="encuesta.php">pepe</a></li>
+            <li>pepe</li>
+            <li>pepe</li>
+            <li>pepe</li>
+            <li>pepe</li>
+            <li>pepe</li>
+            <li>pepe</li>
+          </ul>
+        </div>
+        <div class="col s9">
+          <div class="row">
+            <div class="col s6">
+                hola que tal
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+
+
+  </main>
+
 
 
 
@@ -70,19 +112,13 @@
           </div>
         </div>-->
 
-        <div class="menu">
-          <!--<div class="col l6 s12 m6"><span class="flow-text">I am always full-width (col s12)</span></div>
+        <!--<div class="menu">
+          <div class="col l6 s12 m6"><span class="flow-text">I am always full-width (col s12)</span></div>
           <div class="col l6 m6 s12"><span class="flow-text">I am full-width on mobile (col s12 m6)</span></div>
-        -->
+
           <p>hola</p>
         </div>
-
-
-
-
-
-
-
+      -->
 
 
 
