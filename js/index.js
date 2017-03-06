6d37ce96142ae33bf9b17be75a59602b8ed9d82f
Alexander Cole Mora <alu0100767421@ull.edu.es>
2017-03-06 09:15:29 +0000

    Arreglado por fin (definitivamente) imagen del ... fondo :v:

--- js/index.js
+++ js/index.js
@@ -1,5 +1,31 @@
 $(document).ready(function() {
+  updateFondo();
+  // Y tambien cada vez que se redimensione el navegador
+  $(window).bind("resize", function() {
+    updateFondo();
+  });
   $(".button-collapse").sideNav();
   $('select').material_select();
 
 });
+
+function updateFondo(){
+  sizeWidth = $(window).width();
+  sizeHeight = $(window).height();
+
+  var fondo = jQuery("#imagen-fondo");
+
+  var ratio=1;
+  if(sizeWidth/sizeHeight > ratio){
+    $(fondo).height("100%");
+    $(fondo).width("auto");
+  } else {
+    $(fondo).height("auto");
+    $(fondo).width("100%");
+  }
+
+  // Si a la imagen le sobra anchura, la centramos a mano
+  if ($(fondo).width() > 0) {
+    $(fondo).css('left', (sizeWidth - $(fondo).width()) / 2);
+  }
+};
