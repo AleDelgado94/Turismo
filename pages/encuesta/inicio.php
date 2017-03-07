886bd050c9b7ee8007445d03c933fb0671ee3018
Ale Delgado Martel <alejandrodelgadomartel@gmail.com>
2017-03-07 13:41:44 +0000

    paginacion

--- pages/encuesta/inicio.php
+++ pages/encuesta/inicio.php
@@ -76,14 +76,33 @@
           </form>
         </div>
         <div class="col s9 ">
-
           <div class="center">
 
+            <ul class="pagination">
+              <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+              <li class="active"><a href="#!">1</a></li>
+              <li class="waves-effect"><a href="#!">2</a></li>
+              <li class="waves-effect"><a href="#!">3</a></li>
+              <li class="waves-effect"><a href="#!">4</a></li>
+              <li class="waves-effect"><a href="#!">5</a></li>
+              <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+            </ul>
+
+            <h5>Visita</h5>
 
 
-            <img id="escudo_principal" src="../../images/Escudos/EscudoLineas.png" alt="Escudo Guía de Isora" />
 
 
+
+            <ul class="pagination">
+              <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+              <li class="active"><a href="#!">1</a></li>
+              <li class="waves-effect"><a href="#!">2</a></li>
+              <li class="waves-effect"><a href="#!">3</a></li>
+              <li class="waves-effect"><a href="#!">4</a></li>
+              <li class="waves-effect"><a href="#!">5</a></li>
+              <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+            </ul>
 
 
           </div>
