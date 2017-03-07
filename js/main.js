0319236150c92383b74b19f6c60c4235b55ccdfc
Alexander Cole Mora <alu0100767421@ull.edu.es>
2017-03-07 09:15:39 +0000

    Cambios

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
   $(".button-collapse").sideNav();
+  quitarescudo();
 });
+
+
+
+function quitarescudo(){
+
+
+  if (screen.width < 955) 
+    document.getElementById('escudo-nav').style.display = "none";
+}
