0bf0f3fcf3f2218a940900ab71b5e25bcb55676d
Alexander Cole Mora <alu0100767421@ull.edu.es>
2017-03-09 08:25:05 +0000

    Cambios

--- pages/encuesta/inicio.php
+++ pages/encuesta/inicio.php
@@ -98,7 +98,11 @@
 
             </div>
             <div id="test2" class="col s12">
-
+                <div class="row">
+                  <div class="col s12 m3 l3">
+                    
+                  </div>
+                </div>
 
 
 
