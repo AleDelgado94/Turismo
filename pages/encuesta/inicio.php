6a0c2e8f42934a64037d37d48995b48141051996
Ale Delgado Martel <alejandrodelgadomartel@gmail.com>
2017-03-09 08:08:40 +0000

    espacio encuesta

--- pages/encuesta/inicio.php
+++ pages/encuesta/inicio.php
@@ -89,12 +89,32 @@
             <div id="test1" class="col s12">
 
 
+
+
+
+
+
+
+
             </div>
             <div id="test2" class="col s12">
 
 
+
+
+
+
+
+
             </div>
             <div id="test3" class="col s12">
+
+
+
+
+
+
+
 
 
             </div>
