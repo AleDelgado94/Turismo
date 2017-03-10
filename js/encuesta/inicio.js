4f2793ae38c2105a2fcae982ed2d92fd46543f88
Alexander Cole <alu0100767421@ull.edu.es>
2017-03-10 10:10:23 +0000

    arreglando el calendario y la deseleccion de radiobuttons en el fichero de inicio.js

--- js/encuesta/inicio.js
+++ js/encuesta/inicio.js
@@ -24,6 +24,15 @@ $( "#datepicker" ).datepicker({
 });
 
 
+var era;
+var previo=null;
+function uncheckRadio(rbutton){
+  if(previo &&previo!=rbutton){previo.era=false;}
+  if(rbutton.checked === true && rbutton.era === true){rbutton.checked=false;}
+  rbutton.era=rbutton.checked;
+  previo=rbutton;
+}
+
     $('#add_user').click(function(){
 
       document.write(
