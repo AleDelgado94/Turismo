ca5252153f225ac0cc94e9d23c26c734e53a8d49
Ale Delgado Martel <alejandrodelgadomartel@gmail.com>
2017-03-13 11:59:36 +0000

    quitando comentario

--- pages/encuesta/inicio.php
+++ pages/encuesta/inicio.php
@@ -39,7 +39,7 @@
 
     <nav class="#424242 grey darken-3 top-nav">
       <div class="container-fluid">
-        <div class="nav-wrapper">     //document.getElementById('user').appendChild(newscript);
+        <div class="nav-wrapper">     
 
           <a class="brand-logo left"><img id="escudo-nav" src="../../images/Escudos/Escudo_AyuntamientoGuiadeIsora1.png" alt="Escudo Guía de Isora"></a>
           <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
