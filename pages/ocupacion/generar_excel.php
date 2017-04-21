caa6bea5d4544774018037a268f05f617abab4df
Ale Delgado Martel <alejandrodelgadomartel@gmail.com>
2017-04-21 13:15:28 +0100

    excel

--- pages/ocupacion/generar_excel.php
+++ pages/ocupacion/generar_excel.php
@@ -16,7 +16,27 @@ $excel->getProperties()->setTitle("Ocupación");
 
 
 $excel->setActiveSheetIndex(0);
-$excel->getActiveSheet()->SetCellValue('A1', 'Prueba excel');
+$excel->getActiveSheet()->SetCellValue('A1', 'Media anual de cada hotel');
+$excel->getActiveSheet()->SetCellValue('B1', 'Año');
+$excel->getActiveSheet()->SetCellValue('B2', $_POST['year_ocu']);
+$excel->getActiveSheet()->SetCellValue('D1', 'Hoteles');
+$excel->getActiveSheet()->SetCellValue('D2', 'Media %');
+
+$excel->getActiveSheet()->SetCellValue('E1', 'Allegro Isora');
+$excel->getActiveSheet()->SetCellValue('E2', $_POST['por_Allegro']);
+
+$excel->getActiveSheet()->SetCellValue('F1', 'Bahía Flamengo');
+$excel->getActiveSheet()->SetCellValue('F2', $_POST['por_Flamengo']);
+
+$excel->getActiveSheet()->SetCellValue('G1', 'Ritz Carlton Abama');
+$excel->getActiveSheet()->SetCellValue('G2', $_POST['por_Abama']);
+
+$excel->getActiveSheet()->SetCellValue('H1', 'Palacio Isora');
+$excel->getActiveSheet()->SetCellValue('H2', $_POST['por_Palacio']);
+
+$excel->getActiveSheet()->SetCellValue('J1', 'Media total');
+$excel->getActiveSheet()->SetCellValue('J2', $_POST['media_total']);
+
 
 
 $excel->getActiveSheet()->setTitle('Simple');
