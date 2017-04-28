ab24c71ddfff4ea821df5ae74efdaf9707a64d8f
Alexander Cole <alu0100767421@ull.edu.es>
2017-04-28 08:53:54 +0100

    arreglando posicion boton de añadir personas

--- pages/encuesta/inicio.php
+++ pages/encuesta/inicio.php
@@ -267,13 +267,12 @@
                             <div class="col s12 m6 l6 left-align">
                               <a class="waves-effect waves-light btn #4caf50 green " id="add_user">Añadir</a>
                             </div>
-
-                            <div class="col s12 m6 l6">
+                          </div>
+                          <div class="row">
+                            <div class="col s12 m6 l6 left-align">
                               <a class="waves-effect waves-light btn #f44336 red" id="rm_user">Borrar</a>
                             </div>
                           </div>
-
-
                         </div>
 
 
